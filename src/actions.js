f22666fe76f5ad99421cc3cd3a9664c865dc9cdb
Aidan Starke <starke.aidan@gmail.com>
2021-02-12 13:36:45 +1300

    add refresh action

--- src/actions.js
+++ src/actions.js
@@ -1,5 +1,6 @@
 export const SET_CRYPTO = 'SET_CRYPTO'
 export const CHANGE_PAGE = 'CHANGE_PAGE'
+export const REFRESH = 'REFRESH'
 
 export function setName(name) {
     return {
@@ -12,5 +13,11 @@ export function changePage(page) {
     return {
         type: CHANGE_PAGE,
         page
+    }
+}
+
+export function refresh() {
+    return {
+        type: REFRESH
     }
 }
