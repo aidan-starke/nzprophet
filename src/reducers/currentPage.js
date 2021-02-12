2c3ee70c4f1c5e03c5aba2356fdfb93d99a77598
Aidan Starke <starke.aidan@gmail.com>
2021-02-12 13:38:15 +1300

    add refresh case

--- src/reducers/currentPage.js
+++ src/reducers/currentPage.js
@@ -1,9 +1,11 @@
-import { CHANGE_PAGE } from '../actions'
+import { CHANGE_PAGE, REFRESH } from '../actions'
 
 function currentPage (state = 'Home', action) {
   switch (action.type) {
     case CHANGE_PAGE:
       return action.page
+    case REFRESH:
+      return 'Home'
     default:
       return state
   }
