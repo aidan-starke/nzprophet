07613b956e0c1928d74fa2e02bc6f6c7bde76e9f
Aidan Starke <starke.aidan@gmail.com>
2021-02-12 13:37:56 +1300

    add invested & owned datasets

--- src/reducers/crypto.js
+++ src/reducers/crypto.js
@@ -1,12 +1,14 @@
 import { SET_CRYPTO } from '../actions'
 
-const prices = { 'ALGO': 1, 'DOGE': 0.9, 'ETH': 2350, 'XRP': 1}
-const images = { 'ALGO': './Algorand.png', 'DOGE': './Dogecoin.png', 'ETH': './Ethereum.png', 'XRP': './Ripple.png'}
+const prices = { 'ADA': 1.28, 'ALGO': 1.69, 'DOGE': 0.095857, 'LTC': 257.99, 'XRP': 0.722694}
+const invested = { 'ADA': 200,'ALGO': 200, 'DOGE': 200, 'LTC': 200, 'XRP': 200}
+const owned = {'ADA': 250, 'ALGO': 102.27108, 'DOGE': 1775.87926461, 'LTC': 0.6858259, 'XRP': 503.143395}
+const images = { 'ADA': './Cardano.png', 'ALGO': './Algorand.png', 'DOGE': './Dogecoin.png', 'LTC': './Litecoin.png', 'XRP': './Ripple.png'}
 
 function setCrypto(state = {'name': '', 'price': 0, 'img': ''}, action) {
     switch (action.type) {
         case SET_CRYPTO:
-            return {'name': state.name, 'price': prices[action.name], 'img': images[action.name]}
+            return {'name': action.name, 'price': prices[action.name], 'invested': invested[action.name], 'owned': owned[action.name], 'img': images[action.name]}
         default:
             return state
     }
