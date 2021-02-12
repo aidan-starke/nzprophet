75aaad2843e3c6b75bc7d6e2a244b21a2c103bdc
Aidan Starke <starke.aidan@gmail.com>
2021-02-13 07:19:19 +1300

    refactor, add price alert

--- src/components/ProfitCalculator.jsx
+++ src/components/ProfitCalculator.jsx
@@ -16,27 +16,24 @@ const ProfitCalculator = ({ crypto }) => {
     const { img, name, owned, price, value } = crypto
 
     useEffect(() => {
+        function calculateProfit(currentPrice) {
+            alert(`Current price of ${name}: ${currentPrice}`)
+            const currentValue = roundTo(currentPrice * owned, 2)
+
+            const roundedProfit = roundTo(currentValue - value, 2)
+            setProfit(`$${roundedProfit}`)
+
+            const roundedPercentage = Math.round(roundedProfit / value * 100)
+            setPercentage(`${roundedPercentage}%`)
+
+            toggle(false)
+        }
+
         getPrice(name)
             .then(p => {
                 calculateProfit(p['NZD'])
             })
-    }, [])
-
-
-    function calculateProfit(currentPrice) {
-        alert(`Current price of ${name}: ${currentPrice}`)
-
-        const currentValue = roundTo(currentPrice * owned, 2)
-
-        const roundedProfit = roundTo(currentValue - value, 2)
-        setProfit(`$${roundedProfit}`)
-
-        const roundedPercentage = Math.round(roundedProfit / value * 100)
-        setPercentage(`${roundedPercentage}%`)
-
-        toggle(false)
-    }
-
+    }, [name, owned, value])
 
     return (
         <>
@@ -46,9 +43,9 @@ const ProfitCalculator = ({ crypto }) => {
                     <Card.Body>
                         <Card.Title>{name}</Card.Title>
                         <Card.Text>
-                            You dropped ${value} when {name} was ${roundTo(price, 2)}.
-                        <br />
-                        You currently have {profit} profit ({profitPercentage} ROI). Press Home to refresh.
+                            You invested ${value} when {name} was ${roundTo(price, 2)}.
+                            <br />
+                            You currently have {profit} profit ({profitPercentage} ROI). Press Home to refresh.
                         </Card.Text>
                     </Card.Body>
                 </Card>
