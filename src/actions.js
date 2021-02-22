7b8996c4dea059dbff8139e090b3788e09c10c81
Aidan Starke <starke.aidan@gmail.com>
2021-02-22 22:02:59 +1300

    add setProfitData

--- src/actions.js
+++ src/actions.js
@@ -3,6 +3,7 @@ export const REFRESH = 'REFRESH'
 export const SET_USERS = 'SET_USERS'
 export const SET_USER = 'SET_USER'
 export const SET_USER_DATA = 'SET_USER_DATA'
+export const SET_PROFIT_DATA = 'SET_PROFIT_DATA'
 
 export function changePage(page) {
     return {
@@ -35,6 +36,13 @@ export function setUserData({ username, data }) {
     return {
         type: SET_USER_DATA,
         username: username[0].username,
+        data
+    }
+}
+
+export function setProfitData(data) {
+    return {
+        type: SET_PROFIT_DATA,
         data
     }
 }
