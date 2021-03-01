19c583193d20491e8244ae08aa7a571a7f0958a8
Aidan Starke <starke.aidan@gmail.com>
2021-03-01 15:49:37 +1300

    deploy

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const express = require('express')
 const server = express()
+require('dotenv').config()
 
 const users = require('./routes/users')
 const crypto = require('./routes/crypto')
@@ -9,9 +10,18 @@ const cors = require('cors')
 
 server.use(cors())
 server.use(express.json())
+
+if (process.env.NODE_ENV === "production") {
+    server.use(express.static(path.join(__dirname, '../public')))
+    server.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "public", "index.html"));
+    });
+}
+
 server.use(express.static(path.join(__dirname, '../public')))
 
 server.use('/api/v1/users', users)
 server.use('/api/v1/crypto', crypto)
+
 
 module.exports = server
