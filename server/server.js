c6d84b0125410e06191d8d437e383c4864bf6348
Aidan Starke <starke.aidan@gmail.com>
2021-03-01 20:35:04 +1300

    prepare to deploy

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const server = express()
 
@@ -8,8 +9,16 @@ const cors = require('cors')
 
 server.use(cors())
 server.use(express.json())
+server.use(express.static(path.join(__dirname, 'build')))
 
 server.use('/api/v1/users', users)
 server.use('/api/v1/crypto', crypto)
+
+if (process.env.NODE_ENV === 'production') {
+    server.use(express.static(path.join(__dirname, 'build')));
+    server.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname + 'build/index.html'));
+    })
+}
 
 module.exports = server
