9786db7dea8068106a85eb9ede659cde5b3711c0
Aidan Starke <starke.aidan@gmail.com>
2021-03-26 15:41:05 +1300

    add SET_USERS_CRYPTO, fix LOAD_TRANSACTIONS

--- src/reducers/crypto.js
+++ src/reducers/crypto.js
@@ -1,12 +1,15 @@
-import { SET_PROFIT_DATA, LOAD_TRANSACTIONS } from '../actions'
+import { SET_PROFIT_DATA, LOAD_TRANSACTIONS, SET_USERS_CRYPTO } from '../actions'
 
-function cryptoReducer(state = { profitData: {}, transactions: '' }, action) {
+function cryptoReducer(state = { profitData: {}, currentCrypto: '' }, action) {
     switch (action.type) {
         case SET_PROFIT_DATA:
             state.profitData = action.data
             return state
+        case SET_USERS_CRYPTO:
+            state.usersCrypto = action.data
+            return state
         case LOAD_TRANSACTIONS:
-            state.transactions = action.crypto
+            state.currentCrypto = action.crypto
             return state
         default:
             return state
