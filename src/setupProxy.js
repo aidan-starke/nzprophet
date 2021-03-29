923dc9b375601a5b51a498bc740a8f9dac23e870
Aidan Starke <52016903+aidan-starke@users.noreply.github.com>
2021-03-30 10:27:37 +1300

    hardcode url

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,18 +1,10 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
-require('dotenv').config()
-
-let target
-if (process.env.NODE_ENV === 'development')
-    target = 'http://localhost:5000/'
-else if (process.env.NODE_ENV === 'production')
-    target = 'https://nzprophet-backend.herokuapp.com'
-
 
 module.exports = function (app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target,
+            target: 'https://nzprophet-backend.herokuapp.com/',
             changeOrigin: true,
         })
     )
