360cff08487bb9d435b8c1aedd47e07a1dca8706
aidan-starke <starke.aidan@gmail.com>
2021-05-25 12:52:31 +1200

    link to starkemedia.com

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const FooterComponent = () => {
     return (
         <Card.Footer>
             <Card.Text align='center'>
-                Developed by <Card.Link href='https://www.facebook.com/starkemedia'>Starke Media</Card.Link>
+                Developed by <Card.Link href='https://starkemedia.com'>Starke Media</Card.Link>
             </Card.Text>
             <Card.Text align='center' className="text-muted">
                 Images from <Card.Link href='https://cryptologos.cc/'>https://cryptologos.cc/</Card.Link>
