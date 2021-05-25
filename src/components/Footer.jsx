8766ce0894b1589262eec22c2e3384303a73f399
aidan-starke <starke.aidan@gmail.com>
2021-05-25 12:58:15 +1200

    open links in new tab

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,10 @@ const FooterComponent = () => {
     return (
         <Card.Footer>
             <Card.Text align='center'>
-                Developed by <Card.Link href='https://starkemedia.com'>Starke Media</Card.Link>
+                Developed by <Card.Link href='https://starkemedia.com' target='_blank'>Starke Media</Card.Link>
             </Card.Text>
             <Card.Text align='center' className="text-muted">
-                Images from <Card.Link href='https://cryptologos.cc/'>https://cryptologos.cc/</Card.Link>
+                Images from <Card.Link href='https://cryptologos.cc/' target='_blank'>https://cryptologos.cc/</Card.Link>
             </Card.Text>
         </Card.Footer>
     )
